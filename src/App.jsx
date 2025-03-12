8be344357b581e7268f68ef1cc5068c613099833
mrehan54 <aburehanrashid@gmail.com>
2025-03-12 05:33:30 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti, quis.</h1>
+     <h2>Rehan</h2>
+     
+    </>
+  )
+}
+
+export default App
